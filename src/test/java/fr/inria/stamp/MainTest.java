44789e3b287b188c02a68947f9e7a92399e202f0
danglotb <bdanglot@gmail.com>
2017-09-10 15:18:11 +0200

    reduce number of literals

--- src/test/java/fr/inria/stamp/MainTest.java
+++ src/test/java/fr/inria/stamp/MainTest.java
@@ -6,6 +6,7 @@ import fr.inria.stamp.tavern.Seller;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,21 +19,16 @@ public class MainTest {
 
 	@Test
 	public void test() throws Exception {
-		Seller seller = new Seller(100, Arrays.asList(new Item("Potion", 5),
-				new Item("Torch", 7),
-				new Item("Beer", 2),
-				new Item("Cheese", 3),
-				new Item("Sword", 25)
-		));
+		Seller seller = new Seller(100, Collections.singletonList(new Item("Potion", 5)));
 		Player player = new Player(1000);
 
 		assertEquals("Player{gold=1000, items=[]}", player.toString());
-		assertEquals("Seller{gold=100, items=[Potion, Torch, Beer, Cheese, Sword]}", seller.toString());
+		assertEquals("Seller{gold=100, items=[Potion]}", seller.toString());
 
 		player.buyItem("Potion", seller);
 
 		assertEquals("Player{gold=995, items=[Potion]}", player.toString());
-		assertEquals("Seller{gold=105, items=[Potion, Torch, Beer, Cheese, Sword]}", seller.toString());
+		assertEquals("Seller{gold=105, items=[Potion]}", seller.toString());
 	}
 
 }
